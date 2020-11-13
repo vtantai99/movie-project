bcf687c0a534ec143d86ffae12eea99be953eaa9
Võ  Tài <tantaivo74@gmail.com>
2020-11-13 21:47:32 +0700

    test

--- src/Components/Carousel/index.jsx
+++ src/Components/Carousel/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
